08db02f9280aaaf56bc69755efc8dd2ecc6b9585
AlessandroGenovese2043 <gnvlsn00e10c351w@studium.unict.it>
2023-02-14 16:51:24 +0100

    File Per Iterazione2

---
+++ ELABORAZIONE/Iterazione2/project/src/com/project/Cliente.java
@@ -0,0 +1,58 @@
+package com.project;
+import java.time.LocalDate;
+
+
+public class Cliente {
+    private String nome,cognome,telefono;
+    private LocalDate dataNascita;
+
+    public Cliente(String nome, String cognome,  LocalDate dataNascita,String telefono) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.telefono = telefono;
+        this.dataNascita = dataNascita;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", dataNascita=" + dataNascita +
+                '}';
+    }
+
+}
---
+++ ELABORAZIONE/Iterazione2/project/src/com/test/CartadiCreditoTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import com.project.CartadiCredito;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartadiCreditoTest {
+    @Test
+    void generaNumeroCartaePin(){
+        CartadiCredito carta = new CartadiCredito(LocalDate.now().plusYears(5));
+        assertNotNull(carta.getNumeroCarta());
+        assertNotNull(carta.getPinCarta());
+        assertNotNull(carta.getScadenza());
+    }
+}
