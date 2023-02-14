af752a2f893ff2ffd80503851f156749a25ff0aa
DavideFerrauto <102170555+DavideFerrauto@users.noreply.github.com>
2023-02-14 16:56:41 +0100

    Testing

--- ELABORAZIONE/Iterazione1/Project/src/com/test/CartadiCreditoTest.java
+++ ELABORAZIONE/Iterazione1/Project/src/com/test/CartadiCreditoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+//import static jdk.nashorn.internal.codegen.LocalVariableTypesCalculator.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartadiCreditoTest {
