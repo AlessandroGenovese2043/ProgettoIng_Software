03434c3b0d848233bf62a777e11a75a27c3acc10
DavideFerrauto <102170555+DavideFerrauto@users.noreply.github.com>
2023-02-21 13:14:48 +0100

    implementazione prestito e registro prestito

--- ELABORAZIONE/Iterazione3/project/src/com/project/Prestito.java
+++ ELABORAZIONE/Iterazione3/project/src/com/project/Prestito.java
@@ -1,4 +1,92 @@
 package com.project;
 
+import java.time.LocalDate;
+
 public class Prestito {
+
+    private static int countId=1;
+    private int idPrestito,durataAnni,tassoInteresse;
+    private boolean sanato;
+    private double ammontare;
+    private LocalDate dataInizio,dataFine;
+
+    public Prestito(int durataAnni, int tassoInteresse,  double ammontare,LocalDate dataFine) {
+        idPrestito=countId;
+        countId++;
+        this.durataAnni = durataAnni;
+        this.tassoInteresse = tassoInteresse;
+        this.sanato = false;
+        this.ammontare = ammontare;
+        this.dataInizio = LocalDate.now();
+        this.dataFine = LocalDate.now().plusYears(durataAnni);
+    }
+
+    public  int getIdPrestito() {
+        return idPrestito;
+    }
+
+    public void setIdPrestito(int idPrestito) {
+        this.idPrestito = idPrestito;
+    }
+
+    public int getDurataAnni() {
+        return durataAnni;
+    }
+
+    public void setDurataAnni(int durataAnni) {
+        this.durataAnni = durataAnni;
+    }
+
+    public int getTassoInteresse() {
+        return tassoInteresse;
+    }
+
+    public void setTassoInteresse(int tassoInteresse) {
+        this.tassoInteresse = tassoInteresse;
+    }
+
+    public boolean isSanato() {
+        return sanato;
+    }
+
+    public void setSanato(boolean sanato) {
+        this.sanato = sanato;
+    }
+
+    public double getAmmontare() {
+        return ammontare;
+    }
+
+    public void setAmmontare(double ammontare) {
+        this.ammontare = ammontare;
+    }
+
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(LocalDate dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public LocalDate getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(LocalDate dataFine) {
+        this.dataFine = dataFine;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestito{" +
+                "id prestito=" + idPrestito +
+                "durataAnni=" + durataAnni +
+                ", tassoInteresse=" + tassoInteresse +
+                ", sanato=" + sanato +
+                ", ammontare=" + ammontare +
+                ", dataInizio=" + dataInizio +
+                ", dataFine=" + dataFine +
+                '}';
+    }
 }
--- ELABORAZIONE/Iterazione3/project/src/com/project/RegistroPrestito.java
+++ ELABORAZIONE/Iterazione3/project/src/com/project/RegistroPrestito.java
@@ -1,4 +1,27 @@
 package com.project;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistroPrestito {
+    private static  RegistroPrestito registroprestito;
+    private Map<Integer, Prestito> elencoPrestiti;
+    public RegistroPrestito() {
+        this.elencoPrestiti=new HashMap<>();
+    }
+
+    public static RegistroPrestito getInstance() {
+        if (registroprestito == null)
+            registroprestito = new RegistroPrestito();
+        else
+            System.out.println("Istanza già creata");
+        return registroprestito;
+    }
+
+    public void add(Prestito p)
+    {
+        this.elencoPrestiti.put(p.getIdPrestito(),p);
+    }
+
+
 }
